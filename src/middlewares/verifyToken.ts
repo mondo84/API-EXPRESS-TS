8fe6e37ecc7b1a3b83a2f08ac07a001399b4833e
yesid davila <elmondoles@gmail.com>
2020-04-18 20:16:02 -0500

    Valida token para todas las peticiones de la app.

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -2,37 +2,36 @@
  * Middleware que verifica el token capturado.
  * Nota: El token se genera con el ID del usuario autenticado.
  */
+import { Request, Response, NextFunction } from 'express'; // Modulo de express.
+import objJwt from 'jsonwebtoken'; // Modulo que Genera y valida token.
 
-import { Request, Response, NextFunction } from 'express'
-import  jwt  from 'jsonwebtoken';
-
+// Metodo middleware. @Params: peticion, respuesta, siguienteFuncion
 let verifyToken = (req: Request, res: Response, next: NextFunction) => {
 
     // Formato del token: Authorization Bearer <access_token>
     const bearerHeader = req.headers['authorization'];
-    // const token = req.headers['x-access-token']; // Ya no se usa esta cabecera. Solo de test
 
-    // console.log('Middleware ' + bearerHeader);
-    // Verifica el token.
-    if ( typeof bearerHeader !== 'undefined'){
+    // Valida el encabezado authorization que trae el token.
+    if ( typeof bearerHeader !== 'undefined') {
+
         const bearer = bearerHeader.split(' '); // Creando el espacio en el token. Devuelve una array. spacio[0], token[1]
         const bearerToken = bearer[1];          // Obtengo el token en el array.
 
-        req.params.token = bearerToken;
-
-        // console.log(bearerToken);
-        next();
+        // Verifica el token capturado por request.
+        objJwt.verify(bearerToken, 'MyclaveSecreta', (err, autData) => {
+            if (err) {
+                return res.status(403).json({ msj: 'Forbidden', detail: err.message }); // Devuelve json.
+            } else {
+                // Datos del token. Casting de object a String. para poder pasar por parametro.
+                req.params.datosToken = JSON.stringify(autData);    // Guarda datos en el objeto request.
+                next(); // Da acceso a la funcion siguiente.
+            }
+        });
+        
     } else {
-        // req.params.token = "parametro guardado en el require";
-        // console.log(req.params.token);
         // res.sendStatus(403); // Forbidden.
-        res.status(403).json({ msj: 'No hay encabezado' });
+        res.status(403).json({ msj: 'Forbidden' });
     }
-
 }
 
 export default verifyToken;
-
-// VALIDAR EL TOKEN EN ESTE MIDDLEWARE. PARA EVITAR VALIDAR EL TOKEN
-// PARA TODAS LAS PETICIONES DE LA APP. BORRAR EL metodo verify() del metodo
-// getUsuario.
