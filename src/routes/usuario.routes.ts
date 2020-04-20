81db415c77d9dc1b6f634d6b42f805fd21fa0fef
yesid davila <elmondoles@gmail.com>
2020-04-20 15:02:21 -0500

    Se agrega ruta para delete y update de usuario

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -3,17 +3,19 @@
  * 
  * */
 import { Router } from 'express';   // Importa Modulo de rutas de express.
-import { createUsuario, sigIn, getUsuario, getUsuarioById } from './../controllers/usuarios.controllers';
-import verifyToken from '../middlewares/verifyToken';
+import { createUsuario, sigIn, getUsuario, getUsuarioById 
+        ,updateUsuario, deleteUsuario} from './../controllers/usuarios.controllers'; // Importa controlador usuario.
+import verifyToken from '../middlewares/verifyToken';   // Importa middleware verifica token.
 
 const objRutasUsuario = Router();   // Instancia de rutas.
 
-// objRutasUsuario.route('/').get(getUsuario);              // Ruta get sin middleware.
 objRutasUsuario.route('/').get(verifyToken, getUsuario);    // Forma larga con middleware.
 //objRutasUsuario.get('/', verifyToken, getUsuario);        // Forma corta con middleware.
 
-objRutasUsuario.route('/:idUsuParam(\\d+)').get(verifyToken, getUsuarioById); // Ruta get con patron de solo numeros enteros
-objRutasUsuario.route('/').post(verifyToken,createUsuario); // Ruta post. Crea usuario.
-objRutasUsuario.route('/sigin').post(sigIn); // Ruta post. Autenticacion.
+objRutasUsuario.route('/:idUsuParam(\\d+)').get(verifyToken, getUsuarioById);   // Ruta get con patron de solo numeros enteros
+objRutasUsuario.route('/').post(verifyToken,createUsuario);                     // Ruta post. Crea usuario.
+objRutasUsuario.route('/sigin').post(sigIn);        // Ruta post. Autenticacion.
+objRutasUsuario.route('/:idUsuParam(\\d+)').put(updateUsuario);      // Ruta modificacionde usuario.
+objRutasUsuario.route('/:idUsuParam(\\d+)').delete(deleteUsuario);   // Ruta borra usuario.
 
 export default objRutasUsuario;
