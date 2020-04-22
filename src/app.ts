14340ab3a6d600df38e7938e79fdfa8079a771e1
yesid davila <elmondoles@gmail.com>
2020-04-22 16:59:54 -0500

    Se configura cors() con origen de peticion estatico.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
  * */
 import express , {Application} from 'express';  // Modulo de express. importacion por default y explicita.
 import morgan from 'morgan';                    // Importacion por default
+import cors from 'cors';
 
 // === Importacion por default de los Modulos de rutas.
 import objRutaIndex from './routes/index.routes';
@@ -13,7 +14,7 @@ import objRutaUsuario from './routes/usuario.routes';
 class App {
 
     private app: Application;   // Variable tipo Interface Application de express().
-    
+
     // Ejecuta el servidor. EL puerto es opcional. numero / string
     constructor(private argPort?: number | string) {
         this.app = express();   // Instancia de express.
@@ -24,7 +25,7 @@ class App {
     }
 
     settings(){
-        // Se crea una propiedad 'port' con el valor 
+        // Se crea una propiedad 'port' con el valor
         // pasado en el constructor, sino, por variable de entrono
         // sino, el puerto es 3000 por defecto.
         this.app.set('port', this.argPort || process.env.PORT || 3000 );
@@ -39,18 +40,28 @@ class App {
 
     // Metodo que llama el modulo de rutas.
     routes(){
-       this.app.use(objRutaIndex); 
-       this.app.use('/posts', objRutaPosts ); // Rutas para los posts.
-       this.app.use('/casos', objRutaCaso);         // Rutas para los casos.
-       this.app.use('/usuario', objRutaUsuario);    // Rutas para los usuarios.
+
+        // const whitelist = ['http://localhost:4200', 'http://localhost:6000'];
+
+        /* Configuracion fija(peticiones http) de cors. */
+        const corsOptions = {
+          origin: 'http://localhost:4200',
+          optionsSuccessStatus: 200
+        }
+
+        this.app.use(cors(corsOptions));
+        this.app.use(objRutaIndex);
+        this.app.use('/posts', objRutaPosts );      // Rutas para los posts.
+        this.app.use('/casos', objRutaCaso);        // Rutas para los casos.
+        this.app.use('/usuario', objRutaUsuario);   // Rutas para los usuarios.
     }
-    
+
     public listen() {
         // Escuchador de servidor.
         this.app.listen(this.app.get('port'), ()=>{
             // Muestra mensaje y llama a la propiedad port seteada con el puerto.
             console.log(`Corriendo en el puerto ${this.app.get('port')}`);
-        });  
+        });
     }
 }
 
